787f07a715ba31e9796c4e2b7d503fe62cf943ef
ducanh <anhnd2@ftech.ai>
2021-04-27 11:27:40 +0700

    Not remove stop words and nonsense words, fix bug calculate relevance score

--- main.py
+++ main.py
@@ -3,23 +3,38 @@ import json
 import re
 import numpy as np
 from tqdm import tqdm
+from nltk.corpus import stopwords
+import nltk
+import pprint
+
+nltk.download('stopwords')
+nltk.download('words')
+words = set(nltk.corpus.words.words())
+
+
+def remove_nonsense(review):
+    return " ".join(w for w in nltk.wordpunct_tokenize(review) \
+                    if w.lower() in words or not w.isalpha())
 
 
 def calculate_relevance_score(unique_words, reviews):
-    reviews_safety_hazard = [review for review in reviews if review['Safety hazard'] == 1]
-    reviews_not_safety_hazard = [review for review in reviews if review['Safety hazard'] == 0]
+    reviews_safety_hazard = [review["Review"] for review in reviews if review['Safety hazard'] == 1]
+    reviews_not_safety_hazard = [review["Review"] for review in reviews if review['Safety hazard'] == 0]
 
     word_relevance_score = {}
 
     for word in tqdm(unique_words):
-        A = sum(word in review["Review"] for review in reviews_safety_hazard)
+        A = sum(word in review.split() for review in reviews_safety_hazard)
         C = len(reviews_safety_hazard) - A
 
-        B = sum(word in review["Review"] for review in reviews_not_safety_hazard)
+        B = sum(word in review.split() for review in reviews_not_safety_hazard)
         D = len(reviews_safety_hazard) - B
 
         score = (np.sqrt(A + B + C + D) * (A * D - C * B)) / (np.sqrt((A + B) * (C + D)))
         word_relevance_score[word] = score
+    word_relevance_score = dict(filter(lambda a: a[1] >= 4000, word_relevance_score.items()))
+    word_relevance_score = dict(reversed(sorted(word_relevance_score.items(), key=lambda item: item[1])))
+    pprint.pprint(word_relevance_score)
     return word_relevance_score
 
 
@@ -33,15 +48,22 @@ def preprocess_data(reviews: list):
     for review in reviews:
         ## Lower all review
         review["Review"] = review["Review"].lower()
-        # reviews_lower = [review["Review"].lower() for review in reviews]
 
         ## Remove special character from string
-        regex = r"[^\w+\d+\']+"
-        review["Review"] = re.sub(regex, ' ', review["Review"]).strip()
+        regex1 = r"[^\w+\d+\']+"
+        review["Review"] = re.sub(regex1, ' ', review["Review"]).strip()
+
+        ## Remove nonsense words
+        # review["Review"] = remove_nonsense(review["Review"])
 
     ## Get unique word list
     review_list = [review["Review"].split(" ") for review in reviews]
     unique_words = set().union(*review_list)
+
+    ## Remove stop words
+    # stop_words = set(stopwords.words('english'))
+    # unique_words = unique_words.difference(stop_words)
+    # unique_words = [x for x in unique_words if len(x) > 2]
 
     return unique_words, reviews
 
@@ -57,7 +79,6 @@ def main():
 
         ## Calculate relevance score
         relevance_scrore = calculate_relevance_score(unique_words, reviews)
-        a = 0
     except Exception as e:
         print(e)
 
