55ab7bde714e771018654d43c2c387f019eaf338
ducanh <anhnd2@ftech.ai>
2021-04-27 13:46:15 +0700

    Remove stop words

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import nltk
 import pprint
 
 nltk.download('stopwords')
-nltk.download('words')
 words = set(nltk.corpus.words.words())
 
 
@@ -28,14 +27,15 @@ def calculate_relevance_score(unique_words, reviews):
         C = len(reviews_safety_hazard) - A
 
         B = sum(word in review.split() for review in reviews_not_safety_hazard)
-        D = len(reviews_safety_hazard) - B
+        D = len(reviews_not_safety_hazard) - B
 
         score = (np.sqrt(A + B + C + D) * (A * D - C * B)) / (np.sqrt((A + B) * (C + D)))
         word_relevance_score[word] = score
-    word_relevance_score = dict(filter(lambda a: a[1] >= 4000, word_relevance_score.items()))
-    word_relevance_score = dict(reversed(sorted(word_relevance_score.items(), key=lambda item: item[1])))
-    pprint.pprint(word_relevance_score)
-    return word_relevance_score
+
+    word_relevance_score_4k = dict(filter(lambda a: a[1] >= 4000, word_relevance_score.items()))
+    word_relevance_score_4k = dict(reversed(sorted(word_relevance_score_4k.items(), key=lambda item: item[1])))
+    pprint.pprint(word_relevance_score_4k)
+    return word_relevance_score_4k
 
 
 def preprocess_data(reviews: list):
@@ -53,17 +53,13 @@ def preprocess_data(reviews: list):
         regex1 = r"[^\w+\d+\']+"
         review["Review"] = re.sub(regex1, ' ', review["Review"]).strip()
 
-        ## Remove nonsense words
-        # review["Review"] = remove_nonsense(review["Review"])
-
     ## Get unique word list
     review_list = [review["Review"].split(" ") for review in reviews]
     unique_words = set().union(*review_list)
 
     ## Remove stop words
-    # stop_words = set(stopwords.words('english'))
-    # unique_words = unique_words.difference(stop_words)
-    # unique_words = [x for x in unique_words if len(x) > 2]
+    stop_words = set(stopwords.words('english'))
+    unique_words = unique_words.difference(stop_words)
 
     return unique_words, reviews
 
